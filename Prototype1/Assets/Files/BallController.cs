b7052044e9d6a60111828e8b1100e587cb3227a5
Jean Blignaut <fangspeen@gmail.com>
2018-11-22 22:13:34 +0200

    Physics and materials should take care of this but since I can't figure that out - this seems a reasonable aproximation...

--- Prototype1/Assets/Files/BallController.cs
+++ Prototype1/Assets/Files/BallController.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallController : MonoBehaviour {
+public class BallController : MonoBehaviour
+{
 
     private Rigidbody2D rb;
+
+    AudioSource audioSource;
 
     public Vector3 speed;
     public float MaxXClamp = 1;
@@ -13,15 +16,20 @@ public class BallController : MonoBehaviour {
     public float MinYClamp = 1;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
+
+        audioSource = GetComponent<AudioSource>();
+
         rb = gameObject.GetComponent<Rigidbody2D>();
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         rb.velocity = speed;
         PositionClamp();
-	}
+    }
 
     void PositionClamp()
     {
@@ -29,14 +37,23 @@ public class BallController : MonoBehaviour {
     }
 
     // called when the cube hits the floor
-    void OnCollisionEnter2D(Collision2D col)
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        if(col.gameObject.CompareTag("TopPaddle"))
-        {
-            var relativeSpeed = col.relativeVelocity;
+        Debug.Log("OnCollisionEnter2D");
+
+        var relativeSpeed = collision.relativeVelocity;
+        Debug.Log(collision.relativeVelocity);
+        Debug.Log(string.Format("{0} transform:{1} velocity:{2} angularVelocity:{3}", collision.rigidbody.name, collision.rigidbody.transform, collision.rigidbody.velocity, collision.rigidbody.angularVelocity));
+        Debug.Log(string.Format("{0} transform:{1} velocity:{2} angularVelocity:{3}", collision.otherRigidbody.name, collision.otherRigidbody.transform, collision.otherRigidbody.velocity, collision.otherRigidbody.angularVelocity));
+
+        if (collision.rigidbody.name == "TopSide" || collision.rigidbody.name == "BottomSide")
             relativeSpeed.x *= -1;
-            speed = relativeSpeed;
-            Debug.Log("OnCollisionEnter2D");
-        }
+        else
+            relativeSpeed.y *= -1;
+
+        speed = relativeSpeed;
+
+        if (collision.relativeVelocity.magnitude > 2)
+            audioSource.Play();
     }
 }
